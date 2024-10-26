128cdc709376a07c444a8527f5fb439643650f5b
MrNiNaD <ninadparkar273@gmail.com>
2024-10-26 18:01:22 +0530

    feat: about section styling

--- Frontend/src/component/SchedulerTop.jsx
+++ Frontend/src/component/SchedulerTop.jsx
@@ -7,9 +7,9 @@ const SchedulerTop = () => {
         <span className="one">Book an appointment with</span>
         <span className="two">Physica Physiotherapy</span>
       </h2>
-      <h3>
-        <span>About Physica</span>
-        <span>Choose Therapist &gt;</span>
+      <h3 className="schedule-sub-heading">
+        <span className="about-physica">About Physica</span>
+        <span className="choose-therapist">Choose Therapist &gt;</span>
       </h3>
       <p>
         Our professional doctors will visit your home for an 40 minute treatment
