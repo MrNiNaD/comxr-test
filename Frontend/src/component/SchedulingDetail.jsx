8c4bcb80b21c7568e367f44c964f8f8b13354931
MrNiNaD <ninadparkar273@gmail.com>
2024-10-26 17:51:19 +0530

    icons added in scheduling details

--- Frontend/src/component/SchedulingDetail.jsx
+++ Frontend/src/component/SchedulingDetail.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Stopwatch from "../assets/Stopwatch.png";
+import Vector from "../assets/Vector.png";
+import Badge from "../assets/badge.png";
 
 const SchedulingDetail = () => {
   const bookingDetailConfig = [
@@ -6,17 +9,19 @@ const SchedulingDetail = () => {
       id: 1,
       title: "₹699",
       type: "Fees",
-      icon: <img />,
+      icon: <img src={Vector} />,
     },
     {
       id: 2,
       title: "40min",
       type: "Session Duration",
+      icon: <img src={Stopwatch} />,
     },
     {
       id: 3,
       title: "4.6/5",
       type: "Star rating",
+      icon: <img src={Badge} />,
     },
   ];
 
@@ -25,7 +30,10 @@ const SchedulingDetail = () => {
       {bookingDetailConfig.map((eachDetail) => (
         <button key={eachDetail?.id} className="remove-button-styling">
           <span className="booking-detail-title">{eachDetail?.title}</span>
-          <span className="booking-detail-type">{eachDetail?.type}</span>
+          <span className="booking-detail-type">
+            {eachDetail?.icon}
+            {eachDetail?.type}
+          </span>
         </button>
       ))}
     </div>
