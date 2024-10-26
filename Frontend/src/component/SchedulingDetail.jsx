60f6cd5ff3c33a3a7846622a79de114420cbb0cd
MrNiNaD <ninadparkar273@gmail.com>
2024-10-26 18:23:30 +0530

    feat: fees detail style

--- Frontend/src/component/SchedulingDetail.jsx
+++ Frontend/src/component/SchedulingDetail.jsx
@@ -9,26 +9,29 @@ const SchedulingDetail = () => {
       id: 1,
       title: "₹699",
       type: "Fees",
-      icon: <img src={Vector} />,
+      icon: <img className="fees-icon" src={Vector} />,
     },
     {
       id: 2,
-      title: "40min",
+      title: "40mins",
       type: "Session Duration",
-      icon: <img src={Stopwatch} />,
+      icon: <img className="stopwatch-icon" src={Stopwatch} />,
     },
     {
       id: 3,
       title: "4.6/5",
       type: "Star rating",
-      icon: <img src={Badge} />,
+      icon: <img className="rating-icon" src={Badge} />,
     },
   ];
 
   return (
     <div className="booking-detail">
       {bookingDetailConfig.map((eachDetail) => (
-        <button key={eachDetail?.id} className="remove-button-styling">
+        <button
+          key={eachDetail?.id}
+          className="remove-button-styling booking-detail-btn"
+        >
           <span className="booking-detail-title">{eachDetail?.title}</span>
           <span className="booking-detail-type">
             {eachDetail?.icon}
